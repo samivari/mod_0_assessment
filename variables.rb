cf0af485e6f66202a2ea9f6dcf7cc42861a26127
Sam Iavri <sammi80134@gmail.com>
2021-09-03 13:23:27 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,13 @@
+movie = "Gone Girl"
+is_entertaining = true
+movie = "Tangled"
+
+num_4 = 4
+num_6 = 6
+product = (num_4 * num_6)
+
+favorite_foods = ["Ramen", "Tacos", "Tikia Massala Chettos"]
+favorite_foods = ["Ramen", "Tacos", "Tikia Massala Chettos"].delete(0)
+
+
+puts "There are #{movie.length} characters in #{movie}"
