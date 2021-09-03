2052ee0851064935189b134f0540383055bc7e9f
Sam Iavri <sammi80134@gmail.com>
2021-09-03 13:39:34 -0600

    Model a dinosaur

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  "name" => 'Stegosaurus',
+  "temporal range" => 'Late Jurassic',
+  "height_in_ft" => 14,
+  "length_in_ft" => 30.5,
+  "is_a_herbivore" => true,
+  "spoke_chinese" => false,
+  "inspired_Godzilla" => ['T-Rex', 'Lguanodon', 'Stegosaurus'],
+  "state_dinosaur" => ['Colorado']
+}
