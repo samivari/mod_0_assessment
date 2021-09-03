56950a993bd7b35ce422509acbbb343604609c25
Sam Iavri <sammi80134@gmail.com>
2021-09-03 13:48:24 -0600

    Model a developer

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "name" => 'Sam Ivari',
+  "location" => 'Denver',
+  "height_in_feet" => 5.3,
+  "weight_in_pounds" => 125,
+  "likes_turing" => true,
+  "wants_to_pass_mod0" => true,
+  "favorite_drinks" => ['water', 'wine', 'kombucha'],
+  "favorite_data_types" =. ['booleans', 'strings']
+}
